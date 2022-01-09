abd813c038fb152db7575c17bd2b32813cebeed8
Rafael Adão <raphinha.oliveira11@gmail.com>
2022-01-09 19:56:57 -0300

    formatando e organizando o código

--- bd.php
+++ bd.php
@@ -8,4 +8,4 @@ $password = "123456abc";
 $dbname = "crud";
 
 $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
+	or die('Could not connect to the database server' . mysqli_connect_error());
